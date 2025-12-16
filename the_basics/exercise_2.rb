8258ea6f84f792c04ce58dda40059c1c7a99ef23
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-16 14:25:04 -0500

    Ask for input and display results

--- the_basics/exercise_2.rb
+++ the_basics/exercise_2.rb
@@ -1,4 +1,28 @@
-puts 1234 / 1000
-puts 1234 / 100 % 10
-puts 1234 / 10 % 10
-puts 1234 % 10
+puts "Enter a 4 digit number: "
+number = gets.chomp
+number = number.to_i
+
+def thousands(number)
+  digit = number / 1000
+  puts digit
+end
+
+def hundreds(number)
+  digit = number / 100 % 10
+  puts digit
+end
+
+def tens(number)
+  digit = number / 10 % 10
+  puts digit
+end
+
+def ones(number)
+  digit = number % 10
+  puts digit
+end
+
+thousands(number)
+hundreds(number)
+tens(number)
+ones(number)
