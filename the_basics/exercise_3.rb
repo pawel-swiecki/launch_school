f287866da99028ee8dc39f9a31bc23116dc6b252
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-16 14:28:30 -0500

    Complete exercise 3 of The Basics chapter

---
+++ the_basics/exercise_3.rb
@@ -0,0 +1,6 @@
+my_movie_list = {Batman: 2029, Superman: 2025, Catwoman: 1999, Supergirl: 2026}
+
+puts my_movie_list[:Batman]
+puts my_movie_list[:Superman]
+puts my_movie_list[:Catwoman]
+puts my_movie_list[:Supergirl]
