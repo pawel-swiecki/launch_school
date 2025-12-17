90c03c5df16c5a6c21ca2900d078f7efa23d4d47
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-17 07:51:49 -0500

    Complete exercise 6 of the basics chapter

---
+++ the_basics/exercise_6.rb
@@ -0,0 +1,13 @@
+puts "Enter a number: "
+num1 = gets.chomp
+
+puts "Enter a second number: "
+num2 = gets.chomp
+
+puts "Enter a final number: "
+num3 = gets.chomp
+
+puts "Here are the squares of the numbers you entered:"
+puts num1.to_f ** 2
+puts num2.to_f ** 2
+puts num3.to_f ** 2
