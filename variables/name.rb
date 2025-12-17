4269927d1e1f49a214097a214af87cb864b42eea
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-17 10:14:50 -0500

    Complete exercise 4 of variables chapter

--- variables/name.rb
+++ variables/name.rb
@@ -1,8 +1,11 @@
-puts "What is your name? "
-name = gets.chomp
+puts "What is your first name? "
+first_name = gets.chomp
 
-greeting = "Hello #{name} it is so nice to meet you!"
+puts "What is your last name? "
+last_name = gets.chomp
+
+greeting = "Hello #{first_name} #{last_name} it is so nice to meet you!"
 
 puts greeting
 
-10.times { puts name }
+10.times { puts "#{first_name} #{last_name}" }
