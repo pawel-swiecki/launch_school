8385b992abd2cae01c652746a0240450fe18d9fc
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-17 10:10:13 -0500

    Complete exercise 2 of variables chapter

---
+++ variables/age.rb
@@ -0,0 +1,12 @@
+puts "What is your age? "
+age = gets.chomp.to_i
+
+age_plus_10 = age + 10
+age_plus_20 = age + 20
+age_plus_30 = age + 30
+age_plus_40 = age + 40
+
+puts "In 10 years you will be #{age_plus_10}"
+puts "In 20 years you will be #{age_plus_20}"
+puts "In 30 years you will be #{age_plus_30}"
+puts "In 40 years you will be #{age_plus_40}"
