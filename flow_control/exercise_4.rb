5af4c6a05a982bcbf94e9e36264a702393da15ed
pawel-swiecki <81473085+pawel-swiecki@users.noreply.github.com>
2025-12-18 11:42:04 -0500

    Complete exercise 4 of the flow control chapter

---
+++ flow_control/exercise_4.rb
@@ -0,0 +1,26 @@
+# Snippet 1
+'4' == 4 ? puts("TRUE") : puts("FALSE")
+# prints "FALSE"
+
+# Snippet 2
+x = 2
+if (3) == (3)
+  puts "Did you get it right?"
+else
+  puts "Did you?"
+end
+# prints "Did you get it right?"
+
+# Snippet 3
+y = 9
+x = 10
+if (10 + 1) <= (9)
+  puts "Alright."
+elsif (11) >= (9)
+  puts "Alright now!"
+elsif (y + 1) == x
+  puts "ALRIGHT NOW!"
+else
+  puts "Alrighty!"
+end
+# prints "Alright now!"
